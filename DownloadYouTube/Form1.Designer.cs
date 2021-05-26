11351901ef012cbf2c022fc202f6f035a9ca7c3c
Chaim B. Sommers <cb@compute.co.il>
2021-05-26 22:13:51 +0300

    Replaced GUI with .net core windows app.

---
+++ DownloadYouTube/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace DownloadYouTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbAudioOnly = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSaveIn = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // cbAudioOnly
+            // 
+            this.cbAudioOnly.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbAudioOnly.AutoSize = true;
+            this.cbAudioOnly.Location = new System.Drawing.Point(701, 32);
+            this.cbAudioOnly.Name = "cbAudioOnly";
+            this.cbAudioOnly.Size = new System.Drawing.Size(84, 19);
+            this.cbAudioOnly.TabIndex = 0;
+            this.cbAudioOnly.Text = "Audio only";
+            this.cbAudioOnly.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(25, 57);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(760, 23);
+            this.txtUrl.TabIndex = 2;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(710, 143);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 27);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Download";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Save In";
+            // 
+            // txtSaveIn
+            // 
+            this.txtSaveIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSaveIn.Location = new System.Drawing.Point(25, 105);
+            this.txtSaveIn.Name = "txtSaveIn";
+            this.txtSaveIn.Size = new System.Drawing.Size(760, 23);
+            this.txtSaveIn.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.BackColor = System.Drawing.Color.Black;
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 186);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(760, 252);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtSaveIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbAudioOnly);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbAudioOnly;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSaveIn;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ DownloadYouTube/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DownloadYouTube
+{
+    public partial class Form1 : Form
+    {
+        private Process _downloadingProcess;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.txtSaveIn.Text = Properties.Settings.Default.SaveInPath;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (this._downloadingProcess is { HasExited: false })
+            {
+                var process = this._downloadingProcess;
+                this._downloadingProcess = null;
+                process.Kill(true);
+
+                this.ShowOutput("THE DOWNLOAD PROCESS HAS BEEN STOPPED.",
+                    Color.OrangeRed);
+                this.btnGo.Text = "Download";
+                this.btnGo.ForeColor = this.ForeColor;
+            }
+            else
+            {
+                this.listView1.Items.Clear();
+                this.btnGo.Text = "Stop";
+                this.btnGo.ForeColor = Color.Red;
+                this.DownloadUrl();
+            }
+        }
+
+        private void DownloadUrl()
+        {
+            _downloadingProcess = new Process
+            {
+                StartInfo = { FileName = "youtube-dl.exe" }
+            };
+            if (this.cbAudioOnly.Checked)
+            {
+                _downloadingProcess.StartInfo.ArgumentList.Add("-x");
+                _downloadingProcess.StartInfo.ArgumentList.Add("--audio-format");
+                _downloadingProcess.StartInfo.ArgumentList.Add("mp3");
+                _downloadingProcess.StartInfo.ArgumentList.Add("--audio-quality");
+                _downloadingProcess.StartInfo.ArgumentList.Add("0");
+            }
+            _downloadingProcess.StartInfo.ArgumentList.Add(this.txtUrl.Text);
+            _downloadingProcess.StartInfo.CreateNoWindow = true;
+            _downloadingProcess.EnableRaisingEvents = true;
+            _downloadingProcess.StartInfo.UseShellExecute = false;
+            _downloadingProcess.StartInfo.RedirectStandardOutput = true;
+            _downloadingProcess.StartInfo.RedirectStandardError = true;
+            _downloadingProcess.OutputDataReceived += delegate (object sender, DataReceivedEventArgs e)
+            {
+                this.Invoke(new Action(() => this.ShowOutput(e.Data, Color.LightBlue)));
+            };
+            _downloadingProcess.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs e)
+            {
+                this.Invoke(new Action(() => this.ShowOutput(e.Data, Color.Red)));
+            };
+            _downloadingProcess.Exited += delegate
+            {
+                if (_downloadingProcess != null)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        this.btnGo.Text = "Download";
+                        this.btnGo.ForeColor = this.ForeColor;
+                        if (_downloadingProcess.ExitCode == 0)
+                        {
+                            this.CopyDownloadedFile();
+                        }
+                    }));
+
+                    _downloadingProcess.Dispose();
+                    _downloadingProcess = null;
+                }
+            };
+            _downloadingProcess.Start();
+            _downloadingProcess.BeginOutputReadLine();
+            _downloadingProcess.BeginErrorReadLine();
+        }
+
+        private void CopyDownloadedFile()
+        {
+            //First we need the file name
+            var p = new Process
+            {
+                StartInfo = { FileName = "youtube-dl.exe" }
+            };
+            p.StartInfo.ArgumentList.Add("--get-filename");
+            p.StartInfo.ArgumentList.Add(this.txtUrl.Text);
+            p.StartInfo.CreateNoWindow = true;
+            p.EnableRaisingEvents = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.OutputDataReceived += delegate (object sender, DataReceivedEventArgs e)
+            {
+                this.Invoke(new Action(() => this.SaveFile(e.Data)));
+            };
+            p.Start();
+            p.BeginOutputReadLine();
+        }
+
+        private void SaveFile(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return;
+            }
+            var filePath = Path.Combine(Environment.CurrentDirectory, data);
+            if (this.cbAudioOnly.Checked)
+            {
+                filePath = Path.Combine(Environment.CurrentDirectory,
+                    Path.GetFileNameWithoutExtension(data) + ".mp3");
+            }
+            if (!File.Exists(filePath))
+            {
+                this.ShowOutput($"[local error]  \"{filePath}\" does not exist.",
+                    Color.Red);
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.txtSaveIn.Text))
+            {
+                Properties.Settings.Default.SaveInPath = this.txtSaveIn.Text;
+                Properties.Settings.Default.Save();
+
+                string newPath = Path.Combine(this.txtSaveIn.Text, Path.GetFileName(filePath));
+                if (File.Exists(newPath) &&
+                    MessageBox.Show($"The file {newPath} already exists.\nOverwrite it?",
+                                  "Save File - Overwrite",
+                                  MessageBoxButtons.YesNo,
+                                  MessageBoxIcon.Question,
+                                  MessageBoxDefaultButton.Button2) != DialogResult.Yes)
+                {
+                    return;
+                }
+                File.Move(filePath, newPath);
+                ShowOutput($"THE FILE \"{newPath}\" HAS BEEN SUCCESSFULLY CREATED...........",
+                    Color.LightGreen);
+                if (File.Exists(newPath) && File.Exists(filePath))
+                {
+                    File.Delete(newPath);
+                }
+            }
+            else
+            {
+                var sfd = new SaveFileDialog()
+                {
+                    FileName = Path.GetFileName(filePath),
+                    InitialDirectory = Environment.CurrentDirectory,
+                    OverwritePrompt = true
+                };
+                if (sfd.ShowDialog() != DialogResult.OK)
+                    return;
+                if (File.Exists(sfd.FileName))
+                {
+                    File.Delete(sfd.FileName);
+                }
+                File.Move(filePath, sfd.FileName);
+                ShowOutput($"THE FILE \"{sfd.FileName}\" HAS BEEN SUCCESSFULLY CREATED...........",
+                    Color.LightGreen);
+                if (File.Exists(filePath) && File.Exists(sfd.FileName))
+                {
+                    File.Delete(filePath);
+                }
+            }
+        }
+
+        private void ShowOutput(string data, Color color)
+        {
+            ListViewItem tb = null;
+            if (!string.IsNullOrEmpty(data) &&
+                (data.StartsWith('\r') || data.StartsWith("[download]")))
+            {
+                var el = this.listView1.Items[^1];
+                if (el.Text.StartsWith("[download]"))
+                {
+                    tb = el;
+                }
+            }
+            if (tb == null)
+            {
+                tb = new ListViewItem
+                {
+                    ForeColor = color
+                };
+                this.listView1.Items.Add(tb);
+            }
+            tb.Text = data;
+            this.listView1.Items[^1].EnsureVisible();
+        }
+    }
+}
